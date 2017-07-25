b8377063cf234e72df77318670253e88e65e30ee
alaazamani <alaa.zamani@gmail.com>
2017-07-25 18:48:39 +0300

    Create, update, delete and some awesome messages

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Post(models.Model):
-	title = models.CharField(max_length=50)
+	title = models.CharField(max_length=100)
 	content = models.TextField()
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse("posts:detail", kwargs={"post_id": self.id})
 
 
 class Recipe(models.Model):
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,52 @@
 from django.shortcuts import render
 from .models import Post
-from django.shortcuts import get_object_or_404
+from .forms import PostForm
+from django.shortcuts import get_object_or_404, redirect
 
-def post(request):
-	obj = get_object_or_404(Post, id=1000000)
+from django.contrib import messages
+
+def post_list(request):
+	obj_list = Post.objects.all()
 	context = {
-		"instance": obj,
-
+		"post_list" : obj_list,
 	}
-	return render(request, 'whatever.html', context)
-# Create your views here.
+	return render(request, 'post_list.html', context)
+
+
+def post_detail(request, post_id):
+	obj = get_object_or_404(Post, id=post_id) 
+	context = {
+		"instance" : obj,
+	}
+	return render(request, 'post_detail.html', context)
+
+def post_create(request):
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "OMG! So cool! You created an object.")
+		return redirect("posts:list")
+	context = {
+		"form":form,	
+	}
+	return render(request, 'post_create.html', context)
+
+def post_update(request, post_id):
+	post_object = get_object_or_404(Post, id=post_id)
+	form = PostForm(request.POST or None, instance =post_object)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "Giving it a second thought?")
+		return redirect("posts:list")
+	context = {
+		"form":form,	
+		"post_object":post_object,
+	}
+	return render(request, 'post_update.html', context)
+
+
+def post_delete(request, post_id):
+	Post.objects.get(id=post_id).delete()
+	messages.warning(request, "Seriously bro?")
+	return redirect("posts:list")
+	
